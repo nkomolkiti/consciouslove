7d92ec4a3a7d68cb5bcefdabd7df14e6db1b16f3
nkomolkiti <nkomolkiti@mica.edu>
2017-06-03 22:48:09 -0400

    dashboard

---
+++ js/reference.js
@@ -0,0 +1,10 @@
+//REFERNCE-------------------------------------
+$('#reference').click( function(){
+	$('#popupRef').addClass('show');
+	$('#popupRef').removeClass('hide');
+});
+
+$('.exitRefBtn').click( function(){
+	$('#popupRef').removeClass('show');
+	$('#popupRef').addClass('hide');
+});
